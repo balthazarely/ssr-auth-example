b46008e7d7a7a7e545a2450310dfda84b866d49a
balthazarely <balthazar.elyj@gmail.com>
2026-03-27 12:01:24 -0600

    change to the logout btn

--- app/(dashboard)/home/logout-button.tsx
+++ app/(dashboard)/home/logout-button.tsx
@@ -6,7 +6,7 @@ import { startTransition } from "react";
 export default function LogoutButton() {
   return (
     <button
-      onClick={() => startTransition(() => logoutAction())}
+      onClick={() => startTransition(async () => { await logoutAction(); })}
       className="rounded-lg border border-red-200 bg-red-50 px-4 py-2 text-sm font-medium text-red-600 transition-colors hover:bg-red-100"
     >
       Sign out
